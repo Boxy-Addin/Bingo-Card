6b1a3ab7326065fdf29187f6562486f9eada43d2
Addin Perez <159739788+Boxy-Addin@users.noreply.github.com>
2025-05-09 14:25:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -267,7 +267,7 @@ document.getElementById("rollBallBtn").addEventListener("click", () => {
             const letter = letters[Math.floor(Math.random() * 5)];
             const range = ranges[letter];
             const number = Math.floor(Math.random() * (range[1] - range[0] + 1)) + range[0];
-            drawnBall = `${letter} - ${number}`;
+            drawnBall = `${letter}-${number}`;
 
         } while (drawnBalls.has(drawnBall) && drawnBalls.size < 75); // avoid duplicates
 
